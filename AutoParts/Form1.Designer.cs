f05d90486dda893db19a6fdecea0a29dcc42d376
FabioAlves2 <fabiomalves@ua.pt>
2024-05-25 21:21:54 +0100

    first commit

--- AutoParts/Form1.Designer.cs
+++ AutoParts/Form1.Designer.cs
@@ -28,12 +28,352 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabControl2 = new TabControl();
+            tabPage7 = new TabPage();
+            label8 = new Label();
+            button1 = new Button();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            monthCalendar1 = new MonthCalendar();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            tabPage8 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            tabPage5 = new TabPage();
+            tabPage6 = new TabPage();
+            textBox6 = new TextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabControl2.SuspendLayout();
+            tabPage7.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Anchor = AnchorStyles.Top;
+            tabControl1.Appearance = TabAppearance.FlatButtons;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Controls.Add(tabPage5);
+            tabControl1.Controls.Add(tabPage6);
+            tabControl1.Font = new Font("Segoe UI", 14F);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Multiline = true;
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(776, 439);
+            tabControl1.SizeMode = TabSizeMode.FillToRight;
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.White;
+            tabPage1.Controls.Add(tabControl2);
+            tabPage1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tabPage1.ForeColor = SystemColors.ControlText;
+            tabPage1.Location = new Point(4, 43);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(768, 392);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Admin";
+            // 
+            // tabControl2
+            // 
+            tabControl2.Controls.Add(tabPage7);
+            tabControl2.Controls.Add(tabPage8);
+            tabControl2.Location = new Point(0, 0);
+            tabControl2.Margin = new Padding(0);
+            tabControl2.Name = "tabControl2";
+            tabControl2.Padding = new Point(0, 0);
+            tabControl2.SelectedIndex = 0;
+            tabControl2.Size = new Size(768, 392);
+            tabControl2.TabIndex = 0;
+            // 
+            // tabPage7
+            // 
+            tabPage7.BackColor = Color.White;
+            tabPage7.Controls.Add(textBox6);
+            tabPage7.Controls.Add(label8);
+            tabPage7.Controls.Add(button1);
+            tabPage7.Controls.Add(textBox5);
+            tabPage7.Controls.Add(textBox4);
+            tabPage7.Controls.Add(textBox3);
+            tabPage7.Controls.Add(textBox2);
+            tabPage7.Controls.Add(textBox1);
+            tabPage7.Controls.Add(label7);
+            tabPage7.Controls.Add(label6);
+            tabPage7.Controls.Add(monthCalendar1);
+            tabPage7.Controls.Add(label5);
+            tabPage7.Controls.Add(label4);
+            tabPage7.Controls.Add(label3);
+            tabPage7.Controls.Add(label2);
+            tabPage7.Controls.Add(label1);
+            tabPage7.Location = new Point(4, 29);
+            tabPage7.Name = "tabPage7";
+            tabPage7.Padding = new Padding(3);
+            tabPage7.Size = new Size(760, 359);
+            tabPage7.TabIndex = 0;
+            tabPage7.Text = "Adicionar";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(331, 140);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 20);
+            label8.TabIndex = 15;
+            label8.Text = "Salário";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.FlatAppearance.BorderColor = Color.Black;
+            button1.FlatAppearance.BorderSize = 3;
+            button1.Location = new Point(624, 316);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 37);
+            button1.TabIndex = 13;
+            button1.Text = "Confirmar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.LightGray;
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Location = new Point(429, 102);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(305, 27);
+            textBox5.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.LightGray;
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Location = new Point(387, 67);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(347, 27);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.LightGray;
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(136, 67);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(178, 27);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.LightGray;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(387, 33);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(347, 27);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.LightGray;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(62, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(252, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(6, 105);
+            label7.Name = "label7";
+            label7.Size = new Size(164, 20);
+            label7.TabIndex = 7;
+            label7.Text = "Data Inicio de Contrato";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(337, 35);
+            label6.Name = "label6";
+            label6.Size = new Size(27, 20);
+            label6.TabIndex = 6;
+            label6.Text = "CC";
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            monthCalendar1.Location = new Point(6, 134);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 70);
+            label5.Name = "label5";
+            label5.Size = new Size(124, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Data Nascimento";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(320, 105);
+            label4.Name = "label4";
+            label4.Size = new Size(103, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Código-Postal";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(320, 70);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Morada";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(560, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Preencha todos estes campos para adicionar um novo administrador para esta loja.\r\n";
+            // 
+            // tabPage8
+            // 
+            tabPage8.BackColor = Color.White;
+            tabPage8.Location = new Point(4, 29);
+            tabPage8.Name = "tabPage8";
+            tabPage8.Padding = new Padding(3);
+            tabPage8.Size = new Size(760, 359);
+            tabPage8.TabIndex = 1;
+            tabPage8.Text = "Lista";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = Color.White;
+            tabPage2.Location = new Point(4, 43);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(768, 392);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Client";
+            // 
+            // tabPage3
+            // 
+            tabPage3.BackColor = Color.White;
+            tabPage3.Location = new Point(4, 43);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(768, 392);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Parts";
+            // 
+            // tabPage4
+            // 
+            tabPage4.BackColor = Color.White;
+            tabPage4.Location = new Point(4, 43);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(768, 392);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Vehicle";
+            // 
+            // tabPage5
+            // 
+            tabPage5.BackColor = Color.White;
+            tabPage5.Location = new Point(4, 43);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Size = new Size(768, 392);
+            tabPage5.TabIndex = 4;
+            tabPage5.Text = "Engine";
+            // 
+            // tabPage6
+            // 
+            tabPage6.BackColor = Color.White;
+            tabPage6.Location = new Point(4, 43);
+            tabPage6.Name = "tabPage6";
+            tabPage6.Size = new Size(768, 392);
+            tabPage6.TabIndex = 5;
+            tabPage6.Text = "tabPage6";
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.LightGray;
+            textBox6.BorderStyle = BorderStyle.FixedSingle;
+            textBox6.Location = new Point(392, 137);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(342, 27);
+            textBox6.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabControl2.ResumeLayout(false);
+            tabPage7.ResumeLayout(false);
+            tabPage7.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private TabPage tabPage6;
+        private TabControl tabControl2;
+        private TabPage tabPage8;
+        private TabPage tabPage7;
+        private Label label1;
+        private Label label2;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private MonthCalendar monthCalendar1;
+        private Label label6;
+        private TextBox textBox1;
+        private Label label7;
+        private Button button1;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label8;
+        private TextBox textBox6;
     }
 }
--- AutoParts/Form1.cs
+++ AutoParts/Form1.cs
@@ -6,5 +6,6 @@ namespace AutoParts
         {
             InitializeComponent();
         }
+
     }
 }
